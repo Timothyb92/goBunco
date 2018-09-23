d9dc2b60dc3f4cde6e8e319a6f7982b496d2af60
Timothy Bedsole <timbedsole@gmail.com>
2018-09-23 14:15:30 -0400

    Added userController.js to controllers

---
+++ controllers/userController.js
@@ -0,0 +1,27 @@
+import db from '../models';
+
+export default {
+  findAll: (req, res) => {
+    db.User
+      .find(req.query)
+      .then(dbUser => res.json(dbUser))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: (req, res) => {
+    db.User
+      .findById(req.params.id)
+      .then(dbUser => res.json(dbUser))
+      .catch(err => res.status(422).json(err));
+  },
+  create: (req, res) => {
+    const user = {
+      _id: req.body._id,
+      userName: req.body.userName,
+      password: req.body.password
+    };
+    db.User
+      .create(user)
+      .then(dbUser => res.json(dbUser))
+      .catch(err => res.status(422).json(err));
+  }
+}
