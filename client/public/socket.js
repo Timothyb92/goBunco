656736e08c7d30b1790d41847a8d89a55855ab12
Timothy Bedsole <timbedsole@gmail.com>
2018-09-26 13:31:23 -0400

    Removed socket.js and removed script tag referencing it in index.html

--- client/public/socket.js
+++
@@ -1,5 +0,0 @@
-// document.addEventListener('DOMContentLoaded', function(){
-//   const socket = io.connect('http://localhost:3001');
-
-//   socket.on('connection', () => console.log(`socket.js socket connected`))
-// });
